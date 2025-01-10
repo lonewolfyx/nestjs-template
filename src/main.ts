8988da197b60b6b069788a86226bb1b855dd02e0
lonewolfyx <877085893@qq.com>
2025-01-10 14:03:58 +0800

    fix: 优化初始化，添加打印启动地址

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './module/app.module';
-import { VersioningType } from '@nestjs/common';
-import { RequestIdMiddleware } from './middleware/request.id.middleware';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './module/app.module';
+import {VersioningType} from '@nestjs/common';
+import {RequestIdMiddleware} from './middleware/request.id.middleware';
+import * as os from "node:os";
 
-async function bootstrap() {
+(async () => {
     const app = await NestFactory.create(AppModule);
+    app.enableCors();
 
     // 开启多版本支持
     app.enableVersioning({
@@ -14,7 +16,22 @@ async function bootstrap() {
     // 创建请求 id
     app.use(RequestIdMiddleware);
 
-    await app.listen(process.env.PORT ?? 3000);
-}
+    await app.listen(process.env.PORT ?? 3000)
 
-bootstrap();
+    const port = app.getHttpServer().address().port;
+
+    Object.values(os.networkInterfaces())
+        .flatMap((nInterface) => nInterface ?? [])
+        .filter(
+            (detail) =>
+                detail.address &&
+                (detail.family === 'IPv4' ||
+                    // @ts-expect-error Node 18.0 - 18.3 returns number
+                    detail.family === 4),
+        )
+        .forEach(detail => {
+            console.info(`  ➜  Network: http://${detail.address}:${port}`)
+        })
+
+})()
+
