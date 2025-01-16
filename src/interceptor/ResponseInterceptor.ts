1139441a5428471e2ae17c58e27cfcb97274aa82
lonewolfyx <877085893@qq.com>
2025-01-16 10:16:16 +0800

    fix: 修复 nestjs post 请求时候想要的 status code

--- src/interceptor/ResponseInterceptor.ts
+++ src/interceptor/ResponseInterceptor.ts
@@ -1,8 +1,8 @@
-import {Injectable, NestInterceptor} from '@nestjs/common';
+import {HttpStatus, Injectable, NestInterceptor} from '@nestjs/common';
 import {ExecutionContext} from '@nestjs/common/interfaces/features/execution-context.interface';
 import {map, Observable} from 'rxjs';
 import {CallHandler} from '@nestjs/common/interfaces/features/nest-interceptor.interface';
-import {Request} from 'express';
+import {Request, Response} from 'express';
 import {ResponseDto} from '~/dto/response.dto';
 
 /**
@@ -12,6 +12,12 @@ import {ResponseDto} from '~/dto/response.dto';
 export class ResponseInterceptor implements NestInterceptor {
     intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
         const request = context.switchToHttp().getRequest() as Request;
+        const response = context.switchToHttp().getResponse() as Response;
+
+        // 由于 nestjs 默认返回 201 状态码，这里改为 200
+        if (request.method === 'POST' && response.statusCode === HttpStatus.CREATED) {
+            response.status(HttpStatus.OK)
+        }
 
         return next.handle().pipe(map((data) => new ResponseDto(data || [], request.requestId)));
     }
