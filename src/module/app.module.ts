26723b7bc5bb8473614185f45a482052d3524a9a
lonewolfyx <877085893@qq.com>
2025-01-16 12:01:18 +0800

    update: 重命名响应拦截器文件名称

--- src/module/app.module.ts
+++ src/module/app.module.ts
@@ -2,7 +2,7 @@ import {Module} from '@nestjs/common';
 import {OauthModule} from './oauth/oauth.module';
 import {APP_FILTER, APP_GUARD, APP_INTERCEPTOR} from '@nestjs/core';
 import {AllExceptionFilter} from '~/exception/all.exception.filter';
-import {ResponseInterceptor} from '~/interceptor/ResponseInterceptor';
+import {ResponseInterceptor} from '~/interceptor/response.interceptor';
 import {DatabaseModule} from '~/common/database/database.module';
 import {ConfigModule} from "@nestjs/config";
 import {RedisModule} from "~/common/redis/redis.module";
