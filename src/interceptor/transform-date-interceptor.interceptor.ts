974c67af339f62f74a7d66c1642af55f23cf3dd4
lonewolfyx <877085893@qq.com>
2025-01-18 00:06:21 +0800

    fix: 优化分页查询时间拦截

--- src/interceptor/transform-date-interceptor.interceptor.ts
+++ src/interceptor/transform-date-interceptor.interceptor.ts
@@ -1,7 +1,7 @@
-import {CallHandler, ExecutionContext, Injectable, NestInterceptor} from '@nestjs/common';
-import {map, Observable} from 'rxjs';
-import {isObject} from "radash";
-import {transformDate} from "~/utils";
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+import { isObject } from 'radash';
+import { transformDate } from '~/utils';
 
 export interface Response<T> {
     data: T;
@@ -21,17 +21,29 @@ export class TransformDateInterceptorInterceptor<T> implements NestInterceptor<T
 
                         const result = data.data;
 
+                        if('rows' in result){
+                            result.rows.forEach((item: any) => {
+                                if ('create_time' in item) {
+                                    item['create_time'] = transformDate(item.create_time);
+                                }
+
+                                if ('update_time' in item) {
+                                    item['update_time'] = transformDate(item.update_time);
+                                }
+                            });
+                        }
+
                         if ('create_time' in result) {
-                            data['data']['create_time'] = transformDate(data.create_time)
+                            data['data']['create_time'] = transformDate(data.create_time);
                         }
 
                         if ('update_time' in result) {
-                            data['data']['update_time'] = transformDate(data.update_time)
+                            data['data']['update_time'] = transformDate(data.update_time);
                         }
                     }
 
                     return data;
-                })
+                }),
             );
     }
 }
