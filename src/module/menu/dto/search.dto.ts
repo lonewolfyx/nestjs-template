1632984a293aac99787c3524d96fbb676ee28d2c
lonewolfyx <877085893@qq.com>
2025-01-20 17:20:41 +0800

    feat: 新增分页的查询条件

--- src/module/menu/dto/search.dto.ts
+++ src/module/menu/dto/search.dto.ts
@@ -1,4 +1,11 @@
 import { PaginationDto } from '~/dto/pagination.dto';
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { IsIn, IsNumber, IsOptional } from 'class-validator';
 
-export class SearchDto extends PartialType(PaginationDto) {}
+export class SearchDto extends PartialType(PaginationDto) {
+    @ApiProperty({ description: '是否分页' })
+    @IsOptional()
+    @IsNumber()
+    @IsIn([0, 1])
+    pagination: number;
+}
